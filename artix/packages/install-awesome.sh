e8b73826eccf81f4e5c061f5f67f664e7a0a2f6d
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-07 22:49:03 +0100

    Remove 'checkupdates'

--- artix/packages/install-awesome.sh
+++ artix/packages/install-awesome.sh
@@ -2,7 +2,7 @@
 function install_awesome() {
     wget --quiet https://tools.suckless.org/dmenu/scripts/dmenu_run_with_command_history/dmenu_run_history -O /bin/dmenu_run_history
     chmod +x /bin/dmenu_run_history
-    echo 'alacritty awesome breeze breeze-gtk breeze-icons dmenu feh lxappearance qt5-base qt6-base unclutter-xfixes-git xbindkeys xdg-desktop-portal xdotool xkeyboard-config xorg-server xorg-xinit xorg-xprop xorg-xrandr zenity copyq xorg-xmodmap redshift scrot topgrade xdg-utils gtk2 gtk3 qt5ct kolourpaint mpv tutanota-desktop-bin world/gnome-keyring aerc pandoc-bin checkupdates+aur'
+    echo 'alacritty awesome breeze breeze-gtk breeze-icons dmenu feh lxappearance qt5-base qt6-base unclutter-xfixes-git xbindkeys xdg-desktop-portal xdotool xkeyboard-config xorg-server xorg-xinit xorg-xprop xorg-xrandr zenity copyq xorg-xmodmap redshift scrot topgrade xdg-utils gtk2 gtk3 qt5ct kolourpaint mpv tutanota-desktop-bin world/gnome-keyring aerc pandoc-bin'
     if [ $INSTALL_CRON -eq 1 ]; then
         echo ' cronie-openrc'
     fi
