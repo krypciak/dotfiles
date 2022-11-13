aa270fe97ed9981e8c5bead5191a0d9927a91983
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-13 19:40:40 +0100

    Add 'Update Arch' desktop file

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -39,8 +39,7 @@ SYMLINK_FROM_TO=(
     ".mozilla/icecat/rgvol6f2.default/sessionCheckpoints.json" ""
     "wallpapers"            ".config"
     "rofi"                  ".config"
-    "applications/tutanota-desktop.desktop"      ".local/share/applications"
-    "applications/arch-update.desktop"           ".local/share/applications"
+    "applications"          ".local/share/applications/"
 )
 
 
