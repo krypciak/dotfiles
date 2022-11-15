5133906cd96109fe523f4fa14ae8d3eca5ad8a6c
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-15 14:30:58 +0100

    Add xsel to package list

--- artix/packages/install-awesome.sh
+++ artix/packages/install-awesome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function install_awesome() {
-    echo 'alacritty awesome breeze breeze-gtk breeze-icons feh lxappearance qt5-base qt6-base unclutter-xfixes-git xbindkeys xdg-desktop-portal xdotool xkeyboard-config xorg-server xorg-xinit xorg-xprop xorg-xrandr zenity xorg-xmodmap redshift scrot topgrade xdg-utils gtk2 gtk3 qt5ct kolourpaint mpv tutanota-desktop-bin world/gnome-keyring aerc pandoc-bin'
+    echo 'alacritty awesome breeze breeze-gtk breeze-icons feh lxappearance qt5-base qt6-base unclutter-xfixes-git xbindkeys xdg-desktop-portal xdotool xkeyboard-config xorg-server xorg-xinit xorg-xprop xorg-xrandr zenity xorg-xmodmap redshift scrot topgrade xdg-utils gtk2 gtk3 qt5ct kolourpaint mpv tutanota-desktop-bin world/gnome-keyring aerc pandoc-bin xsel'
     if [ $INSTALL_CRON -eq 1 ]; then
         echo ' cronie-openrc'
     fi
