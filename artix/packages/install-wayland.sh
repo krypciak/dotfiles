bdfae1df5e6067c7366925c9f33ae7ab7e3125ae
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-16 21:15:55 +0100

    Update install-wayland.sh

--- artix/packages/install-wayland.sh
+++ artix/packages/install-wayland.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 function install_wayland() {
-    echo 'wlroots wayland-protocols wayland gammastep inotify-tools fuzzel cliphist swaylock swww'
+    echo 'wlroots wayland-protocols wayland xorg-xwayland gammastep inotify-tools fuzzel cliphist swaylock swww'
 }
