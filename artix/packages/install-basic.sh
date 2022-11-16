699c5cd51dc4b563137a7fc2077ba576b9ab11c9
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-16 19:29:11 +0100

    Update install-basic.sh

--- artix/packages/install-basic.sh
+++ artix/packages/install-basic.sh
@@ -11,5 +11,3 @@ function configure_basic() {
     rc-update add greetd default
     rc-update del agetty.tty1 default
 }
-
-#world/libsamplerate world/freetype2 world/libglvnd world/libogg world/flac world/pipewire-jack world/sbc world/libldac world/libfreeaptx galaxy/libfdk-aac galaxy/lilv galaxy/serd galaxy/sord galaxy/sratom world/webrtc-audio-processing world/libass world/libbluray world/dav1d world/rav1e world/librsvg world/libva world/vid.stab world/libvpx world/x264 world/l-smash world/x265 world/xvidcore world/zimg world/mailcap lib32/lib32-freetype2 lib32/lib32-libglvnd lib32/lib32-dbus world/geoclue world/wolfssl world/gnome-keyring lsd keepassxc copyq world/xorg-xmodmap redshift galaxy/strawberry scrot playerctl nvim-packer-git bluez bluez-utils galaxy/python-typing_extensions python-pip 
