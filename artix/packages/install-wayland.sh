5449dfeb0acf6fa6c7dc6aaf5c015c0cec250316
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-16 20:54:09 +0100

    Typo fix

--- artix/packages/install-wayland.sh
+++ artix/packages/install-wayland.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 function install_wayland() {
-    echo 'wlroots wayland-protocols wayland-scanner gammastep inotify-tools fuzzel cliphist swaylock sww'
+    echo 'wlroots wayland-protocols wayland gammastep inotify-tools fuzzel cliphist swaylock swww'
 }
