2115af4005b6e4f877f6fa5c5bb1a3b744a18c00
krypek <krypek@tuta.io>
2026-04-22 22:48:09 +0200

    system-install: add hook to override xkeyboard-config caps config file with my own

--- user/.config/scripts/capslockfix.sh
+++ user/.config/scripts/capslockfix.sh
@@ -1,2 +1,2 @@
-doas cp /home/krypek/.config/repohub/distrobridge/profile/common/root/usr/share/kbd/keymaps/us-nocaps.map /usr/share/kbd/keymaps/us-nocaps.map
-doas cp /home/krypek/.config/repohub/distrobridge/profile/common/root/usr/share/X11/xkb/symbols/capslock /usr/share/X11/xkb/symbols/capslock
+doas cp /home/krypek/.config/dotfiles/system-install/profile/common/root/usr/share/kbd/keymaps/us-nocaps.map /usr/share/kbd/keymaps/us-nocaps.map
+doas cp /home/krypek/.config/dotfiles/system-install/profile/common/root/usr/share/X11/xkb/symbols/capslock /usr/share/X11/xkb/symbols/capslock
