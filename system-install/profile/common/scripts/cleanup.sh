a2cdf285bcec59b9b5b89f39905999d26f4d9a2f
krypek <krypek@tuta.io>
2026-04-23 16:17:28 +0200

    system-install: update

--- system-install/profile/common/scripts/cleanup.sh
+++ system-install/profile/common/scripts/cleanup.sh
@@ -3,3 +3,9 @@
 info 'Cleaning up'
 
 mkdir -p /mnt/pen /mnt/hdd /mnt/ssd /mnt/share /mnt/redpen /mnt/blackpen
+
+mkdir -p "$USER_HOME/Temp" "$USER_HOME/Documents" "$USER_HOME/Downloads" "$USER_HOME/Music" "$USER_HOME/Pictures" "$USER_HOME/Videos"
+chown_user "$USER_HOME"/*
+
+info "Removing package cache"
+paru --noconfirm -Scc
