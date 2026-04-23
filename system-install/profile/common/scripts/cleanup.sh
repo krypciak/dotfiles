53bfd203621649ca61cacb42dd9dcd7a5e4ef0a9
krypek <krypek@tuta.io>
2026-04-23 20:16:58 +0200

    system-install: only remove pacman cache if building iso

--- system-install/profile/common/scripts/cleanup.sh
+++ system-install/profile/common/scripts/cleanup.sh
@@ -7,7 +7,7 @@ mkdir -p /mnt/pen /mnt/hdd /mnt/ssd /mnt/share /mnt/redpen /mnt/blackpen
 mkdir -p "$USER_HOME/Temp" "$USER_HOME/Documents" "$USER_HOME/Downloads" "$USER_HOME/Music" "$USER_HOME/Pictures" "$USER_HOME/Videos"
 chown_user "$USER_HOME"/*
 
-if [ "$DEV" != '1' ]; then
+if [ "$TYPE" = 'iso' ] && [ "$DEV" != '1' ]; then
     info "Removing package cache"
     paru --noconfirm -Scc
     rm -rf /var/cache/pacman/pkg /root/.cache/paru "$USER_HOME"/.cache/paru
