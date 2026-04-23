dc9aa2f326ec7b3136e4ef355b90f417397974c3
krypek <krypek@tuta.io>
2026-04-23 18:43:00 +0200

    system-install: disable iso service

--- system-install/profile/iso/configs/arch-iso-service.sh
+++ system-install/profile/iso/configs/arch-iso-service.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -a
 USER1="$(ls /home/)"
-USER_HOME="$USER_HOME/home"
+USER_HOME="/home/$USER1"
 
-_git_pull() {
-    cd $USER_HOME/.config/dotfiles || eend 1
-    doas -u "$USER1" git pull
-}
-_git_pull &
-
-_pacman_init() {
-    pacman -Sy > /dev/null 2>&1
-    pacman-key --init > /dev/null 2>&1
-    pacman-key --populate > /dev/null 2>&1
-}
-_pacman_init
+# _git_pull() {
+#     cd $USER_HOME/.config/dotfiles || eend 1
+#     doas -u "$USER1" git pull
+# }
+# _git_pull &
+#
+# _pacman_init() {
+#     pacman -Sy > /dev/null 2>&1
+#     pacman-key --init > /dev/null 2>&1
+#     pacman-key --populate > /dev/null 2>&1
+# }
+# _pacman_init
