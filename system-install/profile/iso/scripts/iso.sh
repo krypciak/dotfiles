42eef65556f82f70c4395107c5236dcdc17670ea
krypek <krypek@tuta.io>
2026-04-23 20:43:47 +0200

    system-install: remove WINIT_X11_SCALE_FACTOR export on iso

--- system-install/profile/iso/scripts/iso.sh
+++ system-install/profile/iso/scripts/iso.sh
@@ -14,6 +14,4 @@ cp "$DOTDIR"/system-install/profile/iso/root/etc/doas.conf /etc/doas.conf
 
 printf "#000000" >"$DOTDIR"/user/.config/wallpapers/selected
 
-echo 'export WINIT_X11_SCALE_FACTOR=1.2' >>"$USER_HOME"/.config/at-login.sh
-
 . "$DIR"/run-mkinitcpio.sh
