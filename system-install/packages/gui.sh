c20f16cb802947870097165f5823e81554658afc
krypek <krypek@tuta.io>
2026-04-28 16:50:09 +0200

    system-install: add missing qt theme packages

--- system-install/packages/gui.sh
+++ system-install/packages/gui.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 arch_gui_install() {
-    echo 'alacritty breeze feh gnome-keyring dolphin'
-    echo 'qt5-base qt5ct qt6-base breeze-gtk breeze-icons gtk2 gtk3 lxappearance'
+    echo 'alacritty feh gnome-keyring dolphin'
+    echo 'qt5-base qt6-base qt5ct qt6ct breeze breeze5 breeze-gtk breeze-icons gtk2 gtk3 lxappearance'
     echo 'xdg-desktop-portal xdg-utils'
 
     echo 'fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-breeze fcitx5-mozc'
