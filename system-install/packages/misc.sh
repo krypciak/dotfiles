f11e4213a2ca0b6d2615f4a28f346c9a3e13c677
krypek <krypek@tuta.io>
2026-04-28 15:23:33 +0200

    system-install: rename security package group to misc

---
+++ system-install/packages/misc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+arch_misc_install() {
+    echo 'keepassxc'
+    echo 'cpu-x'
+    echo 'libfido2 libu2f-server yubikey-manager'
+    echo 'nmap'
+}
--- system-install/packages/security.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-arch_security_install() {
-    echo 'cpu-x keepassxc libfido2 libu2f-server nmap yubikey-manager'
-}
