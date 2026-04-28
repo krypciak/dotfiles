e9da832c8f5016d775ab85f7b57ac1762d7f86f2
krypek <krypek@tuta.io>
2026-04-28 15:16:55 +0200

    system-install: fix virt config using old variable

--- system-install/packages/river.sh
+++ system-install/packages/river.sh
@@ -11,9 +11,8 @@ _configure_river() {
     set +e
     _make_sandbar
     set -e
-    chown "$USER1:$USER_GROUP" -R "$USER_HOME"/.config/river
+    chown_user "$USER_HOME"/.config/river
 }
-
 
 arch_river_install() {
     echo 'river-git xdg-desktop-portal-wlr filtile-git'
@@ -23,4 +22,3 @@ arch_river_configure() {
     _configure_river
     echo
 }
-
