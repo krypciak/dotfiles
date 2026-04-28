1baa6ddd2d5ef2a08c6856d5d2367c200c1da9cb
krypek <krypek@tuta.io>
2026-04-28 20:16:17 +0200

    system-install: remove unnecessary us-nocaps.map

--- user/.config/scripts/capslockfix.sh
+++ user/.config/scripts/capslockfix.sh
@@ -1,2 +1,1 @@
-doas cp /home/krypek/.config/dotfiles/system-install/profile/common/root/usr/share/kbd/keymaps/us-nocaps.map /usr/share/kbd/keymaps/us-nocaps.map
 doas cp /home/krypek/.config/dotfiles/system-install/profile/common/root/usr/share/X11/xkb/symbols/capslock /usr/share/X11/xkb/symbols/capslock
