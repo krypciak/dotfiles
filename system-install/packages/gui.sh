eda53b16f882c272084f09f482a7b9a17b9b964e
krypek <krypek@tuta.io>
2026-04-28 17:38:44 +0200

    system-install: replace lxappearance with nwg-look

--- system-install/packages/gui.sh
+++ system-install/packages/gui.sh
@@ -2,7 +2,7 @@
 
 arch_gui_install() {
     echo 'alacritty feh gnome-keyring dolphin'
-    echo 'qt5-base qt6-base qt5ct qt6ct breeze breeze5 breeze-gtk breeze-icons gtk2 gtk3 lxappearance'
+    echo 'qt5-base qt6-base qt5ct qt6ct breeze breeze5 breeze-gtk breeze-icons gtk2 gtk3 nwg-look'
     echo 'xdg-desktop-portal xdg-utils'
 
     echo 'fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-breeze fcitx5-mozc'
