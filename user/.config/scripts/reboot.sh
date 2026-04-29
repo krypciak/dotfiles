9c36f925d42b96cb547ee395f0a6b3b1b4ce569d
krypek <krypek@tuta.io>
2026-04-29 19:53:50 +0200

    scripts: silence poweroff and reboot scripts

--- user/.config/scripts/reboot.sh
+++ user/.config/scripts/reboot.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if command -v systemctl; then
+if command -v systemctl >/dev/null; then
     if [ -f /etc/iso ]; then
         doas systemctl reboot -ff
     else
         systemctl reboot
     fi
-elif command -v loginctl; then
+elif command -v loginctl >/dev/null; then
     loginctl reboot
 fi
