417b527bac59693fff67bf9ac64c45145364f53a
krypek <krypek@tuta.io>
2026-04-30 19:51:45 +0200

    system-install: fix plymouth script

--- system-install/profile/common/scripts/install-plymouth.sh
+++ system-install/profile/common/scripts/install-plymouth.sh
@@ -17,7 +17,7 @@ if [ "$INSTALL_PLYMOUTH_THEME" = 1 ]; then
     flags="quiet splash vt.global_cursor_default=0 loglevel=3 systemd.show_status=0 rd.udev.log_level=3 fbcon=nodefer"
     sed -i "s|GRUB_CMDLINE_LINUX_DEFAULT=\"|\0 ${flags}|g" /etc/default/grub
 
-    if ! grep -qw "plymouth" /etc/mkinitcio.conf; then
+    if ! grep -qw "plymouth" /etc/mkinitcpio.conf; then
         sed -i 's|udev|\0 plymouth|g' /etc/mkinitcpio.conf
     fi
 fi
