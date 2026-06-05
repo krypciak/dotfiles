de13d0f94ab8eefa72c1e0233421d4d76b890fa5
krypek <krypek@tuta.io>
2026-06-05 20:42:46 +0200

    scripts: fix music playlist download script

--- user/.config/scripts/music-playlist-download.sh
+++ user/.config/scripts/music-playlist-download.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yt-dlp -x --audio-quality 0 -f bestaudio --embed-thumbnail --embed-subs --audio-format mp3 --add-metadata -o '%(artist)s - %(track)s.%(ext)s' "$1"
+yt-dlp -x --audio-quality 0 -f bestaudio --embed-thumbnail --embed-subs --audio-format mp3 --add-metadata -o '%(track)s.%(ext)s' "$1"
